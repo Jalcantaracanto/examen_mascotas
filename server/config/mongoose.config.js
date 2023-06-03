423db232c4233151a0407a980574f3ae430e0474
Javier Alcántara <78369858+Jalcantaracanto@users.noreply.github.com>
2023-06-03 17:03:47 -0400

    Update mongoose.config.js

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1/mascotasdb', {
+    .connect('mongodb://localhost/mascotasdb', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
